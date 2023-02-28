ab05a769a9a0206cef9dea1185ca9322a66c255e
MalgorzataKurtek <kurtek.malgorzata@gmail.com>
2023-02-28 22:45:11 +0100

    first commit

--- TaskManager/src/main/java/pl/coderslab/TaskManager.java
+++ TaskManager/src/main/java/pl/coderslab/TaskManager.java
@@ -1,7 +1,22 @@
 package pl.coderslab;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Arrays;
+import java.util.Scanner;
+
 public class TaskManager {
     public static void main(String[] args) {
+
+        System.out.println(ConsoleColors.BLUE + "Please select an option:");
+        String[][] array = {{"add"}, {"remove"}, {"list"}, {"exit"}};
+        System.out.println(array[0][0]);
+        System.out.println(array[1][0]);
+        System.out.println(array[2][0]);
+        System.out.println(array[3][0]);
+
+        Scanner skan = new Scanner(System.in);
+        String next = skan.next();
 
     }
 }
